b896667c9449dd3b88ab6bf604e02a6594bc2c60
Mehmet Duran Kaya <mehmetdurankaya@gmail.com>
2023-05-09 20:20:26 +0300

    Taksimetre Uygulaması v-1.1 yayınlandı

---
+++ src/main/java/taksimetreapp/TaksimetreApp.java
@@ -0,0 +1,19 @@
+package taksimetreapp;
+
+import java.util.Scanner;
+
+public class TaksimetreApp {
+    public static void main(String[] args) {
+        int km;
+        double perkm=0.20, total, startPrice=10;
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Mesafeyi Km cinsinden giriniz:  ");
+        km=scan.nextInt();
+
+        total=km*perkm;
+        total+=startPrice;
+        total=(total<20)? 20:total;
+        System.out.println("Toplam Tutar: " + total);
+
+    }
+}
